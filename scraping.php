3ec84f78973561416e4297f6a1cd7a912101fbfd
BIGWOJ <wojtek.latos@interia.pl>
2025-01-04 23:47:57 +0100

    Rozpoczęcie funkcji scrapNumerAlbumu

--- scraping.php
+++ scraping.php
@@ -15,11 +15,25 @@ function dbConnection($path){       // funkcja laczenia z baza, zwraca zmienna p
 }
 
 
-function scrapWykladowca($pdo) {       // funkcja scrapowania danych do tabeli wykladowca, argument = zmienna laczenia z baza
+function scrapWykladowca($pdo, $ssl_error=False) {       // funkcja scrapowania danych do tabeli wykladowca, argument = zmienna laczenia z baza
     try {
-        $url = 'https://plan.zut.edu.pl/schedule.php?kind=teacher&query=';                              // link do API
+        $url = 'https://plan.zut.edu.pl/schedule.php?kind=teacher&query=';       // link do API
 
-        $response = file_get_contents($url);                                                            // pobranie zawartosci ze zwrotki z API
+        if ($ssl_error) {
+            $options = [
+                "ssl" => [
+                    "verify_peer" => false,
+                    "verify_peer_name" => false,
+                ],
+            ];
+            $context = stream_context_create($options);
+            #Pobranie zawartosci ze zwrotki z API
+            $response = file_get_contents($url, false, $context);
+        }
+        else {
+            $response = file_get_contents($url);
+        }
+
         echo "Pomyslnie otrzymano zwrot z API \n";
         $data = json_decode($response, true);                                                 // dekodowania zawartosci do JSON
 
@@ -211,14 +225,70 @@ function scrapWydzial($pdo) {       // funkcja scrapowania danych do tabeli wydz
     }
 }*/
 
+function scrapNumerAlbumu($pdo, $ssl_error=False) {
+    try {
+        $url = 'https://plan.zut.edu.pl/schedule_student.php?number={album_index}&start=2024-10-01T00%3A00%3A00%2B01%3A00&end=2025-06-23T00%3A00%3A00%2B01%3A00';
 
+        //Start jako pierwszy dzień zajęć, end jako ostatni dzień zajęć
+        for ($album_index = 53894; $album_index <= 53900; $album_index++) {
+            $url_replaced = str_replace('{album_index}', $album_index, $url);
+            if ($ssl_error) {
+                //Opcje dla file_get_contents, bo bez tego były błędy związane z certyfikatem SSL
+                $options = [
+                    "ssl" => [
+                        "verify_peer" => false,
+                        "verify_peer_name" => false,
+                    ],
+                ];
+                $context = stream_context_create($options);
+                $response = file_get_contents($url_replaced, false, $context);
+            }
+            else {
+                $response = file_get_contents($url_replaced);
+            }
 
+            $data = json_decode($response, true);
+
+            //Warunek gdy chcemy zeby przed scrapowaniem tablica sie czyscila, jesli nie to ustawic na false
+            $clearTableCondition = true;
+            if ($clearTableCondition) {
+                try {
+                    $clearTable = $pdo->prepare("DELETE FROM numerAlbumu");
+                    $clearTable -> execute();
+
+                    $clearIndexes = $pdo->prepare("DELETE FROM sqlite_sequence WHERE name = 'numerAlbumu'");
+                    $clearIndexes -> execute();
+                } catch (PDOException $e) {
+                    echo "Blad zapytania: " . $e -> getMessage();
+                    exit();
+                }
+            }
+
+            //Jeżeli opisany zakres czasowy zwrócił 1 rekord, to znaczy, że nie ma zajęć (pusta lista)
+            if (count($data) == 1) {
+                $puste+=1;
+            }
+            else {
+                $dodac+=1;
+            }
+        }
+        echo "Puste: " . $puste . "\n";
+
+    } catch (PDOException $e) {
+        echo "Blad polaczenia z API: " . $e -> getMessage();
+        exit();
+    }
+}
+
+//Ustawić na True, jeśli występuje błąd związany z certyfikatem SSL
+$ssl_error = True;
 
 $dbPath = 'sqlite:./data.db';   // link do bazy danych
 
 $pdo = dbConnection($dbPath);   // polaczenie z baza danych
 
-#scrapWykladowca($pdo);
+#scrapWykladowca($pdo, $ssl_error);
 
 #scrapWydzial($pdo);
 
+scrapNumerAlbumu($pdo, $ssl_error);
