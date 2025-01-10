3e18b1f6b6791b10fb168bbd5128dc367a30792d
BIGWOJ <wojtek.latos@interia.pl>
2025-01-10 23:42:45 +0100

    Zmiana motywów zrobiona

--- Js.js
+++ Js.js
@@ -1,14 +1,39 @@
-//Czyszczenie inputów po odświeżeniu strony
+//Zmienne odnoszczące się do zmiany motywu strony
+let theme = localStorage.getItem("theme");
+
+//Czynności wykonywane po załadowaniu strony
 document.addEventListener("DOMContentLoaded", () => {
+    //Czyszczenie inputów po odświeżeniu strony
     let filter_inputs = document.querySelectorAll("input");
     filter_inputs.forEach(input => {
         input.value = "";
     });
+
+    //Inicjalizacja motywu na podstawie preferencji systemowych / przeglądarkowych
+    if (!theme) {
+        theme = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light";
+        document.querySelector("body").classList.add(theme);
+    }
+
+    localStorage.setItem("theme", theme);
 });
 
 //Zmiana motywu strony
 document.querySelector(".theme-buttons button:nth-child(1)").addEventListener("click", () => {
-    console.log("Zmiana motywu\nJeszcze nie zrobione");
+    console.log("Zmiana motywu\nJeszcze nie zrobione do końca");
+
+    if (theme === "dark") {
+        document.querySelector("body").classList.remove("dark");
+        document.querySelector("body").classList.add("light");
+        theme = "light";
+    }
+    else {
+        document.querySelector("body").classList.remove("light");
+        document.querySelector("body").classList.add("dark");
+        theme = "dark";
+    }
+
+    localStorage.setItem("theme", theme);
 });
 
 //Zmiana wielkości czcionki
@@ -44,4 +69,10 @@ document.querySelector(".manage-button").addEventListener("click", () => {
     console.log("Zarządzanie kafelkami\nJeszcze nie zrobione");
 });
 
+if (theme === "dark") {
+    document.querySelector("body").classList.add("dark");
+}
 
+if (theme === "light") {
+    document.querySelector("body").classList.add("light");
+}
