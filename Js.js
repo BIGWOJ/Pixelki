e0e06faf925b8cbca41074c4987d6703844ece59
lw53894 <lw53894@zut.edu.pl>
2025-01-15 12:41:53 +0100

    Praca nad strzałką w lewo i poprawnym wyświetlaniem dat w tabeli

--- Js.js
+++ Js.js
@@ -22,7 +22,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
 //Zmiana motywu strony
 document.querySelector(".theme-buttons button:nth-child(2)").addEventListener("click", () => {
-    console.log("Zmiana motywu\nJeszcze nie zrobione do końca");
     if (theme === "dark") {
         document.querySelector("body").classList.remove("dark");
         document.querySelector("body").classList.add("light");
@@ -76,6 +75,7 @@ document.querySelector(".filter-share").addEventListener("click", () => {
 document.querySelector(".range-calendar").addEventListener("click", () => {
     console.log("Kalendarz zakresowy\nJeszcze nie zrobione");
 });
+
 //Zastosowanie filtrów po kliknięciu w przycisk "Filtruj"
 document.querySelector(".filter-buttons button:nth-child(1)").addEventListener("click", () => {
     console.log("Filtrowanie\nJeszcze nie zrobione");
@@ -111,7 +111,7 @@ document.querySelector(".navigation button:nth-child(2)").addEventListener("clic
         <thead>
             <tr>
                 <th></th>
-                <th>Dzisiejsza data</th>
+                <th>${new Date().toLocaleDateString()}</th>
             </tr>
         </thead>`;
 
@@ -138,13 +138,18 @@ document.querySelector(".navigation button:nth-child(3)").addEventListener("clic
     const table = document.querySelector(".calendar_view");
     let connected_table = '';
     let lesson_hours = ['8:30 - 10:00', '10:15 - 12:00', '14:15 - 16:00', '16:15 - 18:00', '18:15 - 19:30']
+    let current_date = new Date();
+    let week_start = current_date.getDate() - current_date.getDay() + 1;
+
     for (let i = 0; i < 7; i++) {
         //Head jednego dnia
+        let date = new Date(current_date.setDate(week_start + i)).toLocaleDateString();
+
         const daily_header = `
             <thead>
                 <tr>
                     <th></th>
-                    <th>Data</th>
+                    <th>${date}</th>
                 </tr>
             </thead>`;
 
@@ -176,21 +181,23 @@ calendar.style.height = `${table.scrollHeight +250}px`;
 //Tygodniowy widok kalendarza
 document.querySelector(".navigation button:nth-child(4)").addEventListener("click", () => {
     const table = document.querySelector(".calendar_view");
+    let current_date = new Date();
+    let week_start = current_date.getDate() - current_date.getDay() + 1;
+    let days_shortcut = ['pon.', 'wt.', 'śr.', 'czw.', 'pt.', 'sob.', 'ndz.'];
 
     //Head tygodniowego widoku
-    const weekly_header = `
+    let weekly_header = `
         <thead>
             <tr>
-                <th></th>
-                <th>poniedziałek</th>
-                <th>wtorek</th>
-                <th>środa</th>
-                <th>czwartek</th>
-                <th>piątek</th>
-                <th>sobota</th>
-                <th>niedziela</th>
-            </tr>
-        </thead>`;
+                <th></th>`;
+
+    for (let i = 0; i < 7; i++) {
+        //Data bez roku
+        let date = new Date(current_date.setDate(week_start + i)).toLocaleDateString(undefined, { month: '2-digit', day: '2-digit' });
+        weekly_header += `<th>${days_shortcut[i]} ${date}</th>`;
+    }
+
+    weekly_header += `</tr></thead>`;
 
     //Body tygodniowego widoku
     let weekly_body = `<tbody>`;
@@ -245,9 +252,88 @@ document.querySelector(".navigation button:nth-child(5)").addEventListener("clic
 
 //Semestralny widok kalendarza
 document.querySelector(".navigation button:nth-child(6)").addEventListener("click", () => {
+    const table = document.querySelector(".calendar_view");
+
+    //Head semestralnego widoku
+    const semester_header = `
+        <thead>
+            <tr>
+                <th></th>
+                <th>poniedziałek</th>
+                <th>wtorek</th>
+                <th>środa</th>
+                <th>czwartek</th>
+                <th>piątek</th>
+                <th>sobota</th>
+                <th>niedziela</th>
+            </tr>
+        </thead>`;
+
+    //Body semestralnego widoku
+    let semester_body = `<tbody>`;
+    for (let i = 0; i < 15; i++) {
+        semester_body += `<tr>`;
+        for (let j = 0; j < 8; j++) {
+            if (j === 0) {
+                semester_body += `<td>${i + 1}</td>`;
+                continue;
+            }
+            semester_body += `<td></td>`;
+        }
+        semester_body += `</tr>`;
+    }
+    semester_body += `</tbody>`;
+
+    table.innerHTML = semester_header + semester_body;
+});
+
+//Strzałka w lewo
+document.querySelector(".calendar_range button:nth-child(1)").addEventListener("click", () => {
+
+    const table = document.querySelector(".calendar_view");
+    let row_count = table.rows.length;
+    console.log(row_count);
+    //Trzeba jakoś inaczej zmieniać dla semestru oraz miesiąca, bo tam nie ma dat, tylko dni
+    //Zmiana dziennego też inaczej, bo tam jest długa tabela
+    if (row_count !== 14) {
+        return;
+    }
+
+    let current_dates = (document.querySelector(".calendar_view thead").innerHTML)
+    .split("</th>")
+    .slice(1, -1)
+    .map(item => {
+        let [day, month] = item.match(/\d{1,2}\.\d{1,2}/)[0].split('.');
+        return `${day.padStart(2, '0')}-${month.padStart(2, '0')}-${new Date().getFullYear()}`;
+    });
+     console.log((current_dates));
+
+     let days_shortcut = ['pon.', 'wt.', 'śr.', 'czw.', 'pt.', 'sob.', 'ndz.'];
+
+    let today_header = `
+    <thead>
+        <tr>
+            <th></th>`;
+
+    for (let i = 0; i < current_dates.length; i++) {
+        let [day, month] = current_dates[i].split("-");
+        let date = new Date(new Date().getFullYear(), month - 1, day);
+        date.setDate(date.getDate() - 7);
+        current_dates[i] = `${date.getDate()}.${(date.getMonth() + 1).toString().padStart(2, '0')}`;
+        today_header += `<th>${days_shortcut[i]} ${current_dates[i]}</th>`;
+    }
+
+    today_header += `</tr></thead>`;
+
+    table.tHead.innerHTML = today_header;
 
 });
 
+
+//Strzałka w prawo
+document.querySelector(".calendar_range button:nth-child(3)").addEventListener("click", () => {
+    console.log("Strzałka w prawo\nJeszcze nie zrobione");
+});
 
 if (theme === "dark") {
     document.querySelector("body").classList.add("dark");
